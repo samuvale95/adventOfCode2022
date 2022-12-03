8226f7f8ca292f7d4520fb401f0a227c4393058e
SDAvalentes <samuele.valente@sdabocconi.it>
2022-12-03 12:30:53 +0100

    update new_file.py

--- src/utils/new_file.py
+++ src/utils/new_file.py
@@ -4,7 +4,7 @@ l = filter(lambda x: "__" not in x and ".py" in x, os.listdir("src"))
 l = list(l)
 n = int(sorted(l)[-1][:2]) + 1 if len(l) > 0 else 1
 
-DEFAULT_FILE = f"from utils.api import get_input\n\ninput_str = get_input({n})\n\n# WRITE YOUR SOLUTION HERE\n\n"
+DEFAULT_FILE = f"from utils.api import get_input\n\ninput_str = get_input({n})\n\n# WRITE YOUR SOLUTION HERE\n\ndef execute_first(input_str):\n\tpass\n\ndef execute_second(input_str):\n\tpass\n\nif __name__ == '__main__':\n\tpass"
 
 path = f"src/{n:02d}.py"
 with open(path, "w") as f:
