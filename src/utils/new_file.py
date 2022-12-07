b72df352847d4a7ab1dc41ea5d774b1a53fcde12
Sule <samuelevalente@gmail.com>
2022-12-07 13:28:48 +0100

    Update new_file.py

--- src/utils/new_file.py
+++ src/utils/new_file.py
@@ -4,7 +4,33 @@ l = filter(lambda x: "__" not in x and ".py" in x, os.listdir("src"))
 l = list(l)
 n = int(sorted(l)[-1][:2]) + 1 if len(l) > 0 else 1
 
-DEFAULT_FILE = f"from utils.api import get_input\n\ninput_str = get_input({n})\n\n# WRITE YOUR SOLUTION HERE\n\ndef execute_first(input_str):\n\tpass\n\ndef execute_second(input_str):\n\tpass\n\nif __name__ == '__main__':\n\tpass"
+DEFAULT_FILE = f"""from utils.api import get_input
+
+input_str = get_input({n})
+input_str_trivial = ''
+
+def execute_first(input_str):
+    pass
+
+def execute_second(input_str):
+    pass
+
+def test_easy_input_first(input_str_trivial):
+	assert execute_first(input_str_trivial) == ""
+    #ADD MORE TESTS HERE
+
+
+def test_easy_input_second(input_str_trivial):
+    assert execute_first(input_str_trivial) == ""
+    #ADD MORE TESTS HERE
+
+if __name__ == '__main__':
+    test_easy_input_first(input_str_trivial)
+	test_easy_input_second(input_str_trivial)
+
+	print(execute_first(input_str))
+	print(execute_second(input_str))
+"""
 
 path = f"src/{n:02d}.py"
 with open(path, "w") as f:
